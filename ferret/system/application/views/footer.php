afe91851865f8804a84e9e3e9569c2d6851a6c4d
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-02-26 14:33:54 +0100

    Update footer.php

--- ferret/system/application/views/footer.php
+++ ferret/system/application/views/footer.php
@@ -4,7 +4,7 @@
 </tr>
 <tr><td colspan="2">
 <div id="footer">
-<p>Institute for Computational Neuroscience UKE Hamburg 2012</p>
+<p>Institute for Computational Neuroscience UKE Hamburg 2012-<?php echo @date('Y'); ?></p>
 </div>
 </td>
 </tr>
