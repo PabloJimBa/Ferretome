a2eb7bdd118dc4787c53be5601f09e391b2e8424
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-02-27 15:34:56 +0100

    Update welcome_view.php

--- ferret/system/application/views/welcome_view.php
+++ ferret/system/application/views/welcome_view.php
@@ -30,6 +30,11 @@
 	<td>
 	<h2>Additional</h2>
 	</td>
+
+	<!-- Manual link -->
+
+	<td>
+	<h3><a href="index.php?c=manual&m=index">How to upload data?</a></h3>
 </tr>
 
 <!-- Main table content -->
@@ -63,15 +68,15 @@
 	
 	<td valign="top">
 	
-	<h2>Literature:</h2>
+	<h3>Literature:</h3>
 	<p><a href="index.php?c=literature&m=search">Show Literature </a></p>
 	<p><a href="index.php?c=authors&m=search">Show Authors </a></p>
 	
-	<h2>Mapping data:</h2>	
+	<h3>Mapping data:</h3>	
 	<p><a href="index.php?c=brainmaps&m=search">Show Brain Maps </a></p>
 	<p><a href="index.php?c=acronyms&m=search">Show Acronyms </a></p>
 	
-	<h2>Expirement data:</h2>
+	<h3>Expirement data:</h3>
 	
 	<p><a href="index.php?c=injections&m=search">Show Injections </a></p>
 	
@@ -84,14 +89,14 @@
 	<td valign="top">
 	
 	
-	<h2>Special tables</h2>
+	<h3>Special tables</h3>
 	<p><a href="index.php?c=menueditor">Main menu editor</a></p>
 	<p><a href="index.php?c=journal">Actions Journal</a></p>
 	<p><a href="index.php?c=codingrules">Coding rules</a></p>
-	<h2>Additional tables for mapping studies</h2>
+	<h3>Additional tables for mapping studies</h3>
 	<p><a href="index.php?c=abbreviations">Literature Abbr.</a></p>
 	<p><a href="index.php?c=parameters">Architecture parametrs </a></p>
-	<h2>Additional tables for injection studies</h2>
+	<h3>Additional tables for injection studies</h3>
 	<p><a href="index.php?c=tracers">Injection tracers</a></p>
 	<p><a href="index.php?c=injectionsparameters"> Injection parameter </a></p>
 	
