3585dc4f48b5ae1e8e6e2bfbd783fd62366f81df
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-03-06 11:12:04 +0100

    Update abbreviations_view.php

--- ferret/system/application/views/abbreviations_view.php
+++ ferret/system/application/views/abbreviations_view.php
@@ -1,132 +1,133 @@
 <?php if (!defined('BASEPATH')) exit('Wir haben Sie nicht verstanden!'); ?>
+
+<!-- Load the header -->
+
 <?php  $this->load->view('header');  ?>
 
 
+<!-- Index -->
 
 <?php if($action == 'index'):?>
 
+	<?php if(isset($index_message)):?>
+		<p><?=$index_message?></p>
+	<?php endif;?>
+
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
+
+	<h1>Journals/ Books / Etc. </h1>
+
+	<h2><a href="index.php?c=abbreviations&m=add">Add new Literature Abbr. </a> &nbsp; | &nbsp; <a href="index.php?c=abbreviations&m=show">Show all </a></h2>
 
 
-<?php if(isset($index_message)):?>
-<p><?=$index_message?></p>
+
 <?php endif;?>
 
-<h2>Journals/ Books / Etc</h2>
 
-<p><a href="index.php?c=abbreviations&m=add">Add new Literature Abbr. </a></p>
-<p><a href="index.php?c=abbreviations&m=show">Show all </a></p>
-
-
-
-<?php endif;?>
-
-
+<!-- Add -->
 
 <?php if($action == 'add'):?>
 
-<p><a href="index.php?c=abbreviations&m=show">Show all </a></p>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
-<h2>new Literature Abbr. </h2>
+	<h1>New Literature Abbr. </h1>
 
+	<p><a href="index.php?c=abbreviations&m=show">Show all </a></p>
 
+	<form method="post" id="frm" name="frm" action="index.php?c=abbreviations&m=insert">
 
-<form method="post" id="frm" name="frm" action="index.php?c=abbreviations&m=insert">
+	<table border="0" cellpadding="3" cellspacing="1">
 
-<table border="0" cellpadding="3" cellspacing="1">
+	<!-- Form -->
 
+	<?php foreach($fields as $field): ?>
 
-<?php foreach($fields as $field): ?>
+		<?php if ($field->primary_key == 1) continue; ?>
 
-<?php if ($field->primary_key == 1) continue; ?>
+			<tr>
+			<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
 
-<tr>
-	<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
-	
-	<?php if ($field->type == 'blob'): ?>
-	<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ></textarea></td>
-	<?php else : ?>
-	<td><input class="input" name="<?php echo $field->name; ?>" value="" size="30" /></td>
-	<?php endif; ?>
-	
-</tr>
-<?php endforeach; ?>
-</table>
+			<?php if ($field->type == 'blob'): ?>
+			<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ></textarea></td>
+			<?php else : ?>
+			<td><input class="input" name="<?php echo $field->name; ?>" value="" size="30" /></td>
+		<?php endif; ?>
 
-<input type="submit" class="submit" value="Insert" />
+		</tr>
+	<?php endforeach; ?>
+	</table>
 
-</form>
+	<input type="submit" class="submit" value="Insert" />
+
+	</form>
 
 <?php endif;?>
 
 
-
-
-
+<!-- Show -->
 
 <?php if($action == 'show'):?>
 
-<p><a href="index.php?c=abbreviations">Back </a></p>
-<p><a href="index.php?c=abbreviations&m=add">Add new Abbr. </a></p>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
-<h2>All Literature Abbr.</h2>
+	<h1>All Literature Abbr.</h1>
 
-<table border="0" cellpadding="3" cellspacing="1">
-<?php foreach($block_data->result() as $bdata): ?>
-<tr>
-	<td><?=$bdata->abbreviations_short?></td>
-	<td><?=$bdata->abbreviations_full?></td> 
-	<td><a href="index.php?c=abbreviations&m=edit&id=<?=$bdata->abbreviations_id?>">edit</a></td>
-</tr>
-<?php endforeach; ?>
-</table>
+	<p><a href="index.php?c=abbreviations&m=add">Add new Literature Abbr. </a></p>
+
+	<!-- Form -->
+
+	<table border="0" cellpadding="3" cellspacing="1">
+	<?php foreach($block_data->result() as $bdata): ?>
+		<tr>
+			<td><?=$bdata->abbreviations_short?></td>
+			<td><?=$bdata->abbreviations_full?></td> 
+			<td><a href="index.php?c=abbreviations&m=edit&id=<?=$bdata->abbreviations_id?>">edit</a></td>
+		</tr>
+	<?php endforeach; ?>
+	</table>
 <?php endif;?>
 
+
+<!-- Edit -->
 
 <?php if($action == 'edit'):?>
 
-<p><a href="index.php?c=abbreviations&m=show">Show all</a></p>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
+	<h1>Edit Literature Abbr.</h1>
 
-<h2>Edit Literature Abbr.</h2>
+	<p><a href="index.php?c=abbreviations&m=show">Show all</a></p>
 
+	<!-- Form -->
 
+	<form method="post" id="frm" name="frm" action="index.php?c=abbreviations&m=update&id=<?=$block_data->abbreviations_id?>">
 
-<form method="post" id="frm" name="frm" action="index.php?c=abbreviations&m=update&id=<?=$block_data->abbreviations_id?>">
+	<table border="0" cellpadding="3" cellspacing="1">
 
-<table border="0" cellpadding="3" cellspacing="1">
+	<?php foreach($fields as $field): ?>
 
+		<?php if ($field->primary_key == 1) continue; ?>
 
+			<tr>
+				<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
 
+				<?php if ($field->type == 'blob'): ?>
+					<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ><?php $f=$field->name; echo form_prep($block_data->$f); ?></textarea></td>
+				<?php else : ?>
+					<td><input class="input" name="<?php echo $field->name; ?>" value="<?php $f=$field->name; echo form_prep($block_data->$f); ?>" size="30" /></td>
+				<?php endif; ?>
 
-<?php foreach($fields as $field): ?>
+			</tr>
+	<?php endforeach; ?>
+	</table>
 
-<?php if ($field->primary_key == 1) continue; ?>
+	<input type="submit" class="submit" value="Update" />
 
-<tr>
-	<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
-	
-	<?php if ($field->type == 'blob'): ?>
-	<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ><?php $f=$field->name; echo form_prep($block_data->$f); ?></textarea></td>
-	<?php else : ?>
-	<td><input class="input" name="<?php echo $field->name; ?>" value="<?php $f=$field->name; echo form_prep($block_data->$f); ?>" size="30" /></td>
-	<?php endif; ?>
-	
-</tr>
-<?php endforeach; ?>
-</table>
-
-<input type="submit" class="submit" value="Update" />
-
-</form>
+	</form>
 
 <?php endif;?>
 
 
-
-
+<!-- Load the footer -->
 
 <?php $this->load->view('footer');
-
-
-
-
