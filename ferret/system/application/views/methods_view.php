93f60d6543db768f56a5f4bcfefff3a7c0f0985a
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-03-09 10:54:34 +0100

    Update methods_view.php

--- ferret/system/application/views/methods_view.php
+++ ferret/system/application/views/methods_view.php
@@ -1,260 +1,272 @@
 <?php if (!defined('BASEPATH')) exit('Wir haben Sie nicht verstanden!'); ?>
+
+<!-- Load the header -->
+
 <?php  $this->load->view('header');  ?>
 
 
+<!-- Index -->
 
 <?php if($action == 'index'):?>
 
-<h1>Welcome to FBBD!</h1>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
-<?php if(isset($index_message)):?>
-<p><?=$index_message?></p>
+	<h1>Injections input</h1>
+
+	<?php if(isset($index_message)):?>
+	<p><?=$index_message?></p>
+	<?php endif;?>
+
+
+	<h2><a href="index.php?c=injections&m=add">Add new Injections </a> &nbsp; | &nbsp; <a href="index.php?c=methods&m=add">Add new Injection Methods </a></h2>
+	<h2><a href="index.php?c=injectionsdata&m=add">Add new Injection Data</a> &nbsp; | &nbsp; <a href="index.php?c=injectionsparameters&m=add">Add new Injection Parameters</a></h2>
+
 <?php endif;?>
 
-<h2>Methods</h2>
 
-<p><a href="index.php?c=methods&m=add">Add new Method </a></p>
-<p><a href="index.php?c=methods&m=show">Show all </a></p>
-
-
-
-<?php endif;?>
-
-
+<!-- Add -->
 
 <?php if($action == 'add'):?>
 
-<p><a href="index.php?c=methods&m=show">Show all </a></p>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
-<h2>New Injection Method </h2>
+	<h1>New Injection Method </h1>
 
+	<h2><a href="index.php?c=methods&m=show">Show all </a></h2>
 
-<a href="#" onclick="show_coding_rules('methods'); return false;">Coding rules for Methods</a>
-<form method="post" id="frm" name="frm" action="index.php?c=methods&m=insert">
+	<!-- Form -->
 
-<table border="0" cellpadding="3" cellspacing="1">
+	<a href="#" onclick="show_coding_rules('methods'); return false;">Coding rules for Methods</a>
+	<form method="post" id="frm" name="frm" action="index.php?c=methods&m=insert">
 
-<tr id="auto_block">
-	<td>Search for publication<br/> for this injection method </td>
-	<td><input title="Please, start to type a title" type="text" id="autocomplite_auth" class="input"/></td>
-</tr>
+	<table border="0" cellpadding="3" cellspacing="1">
 
-<tr style="display:none;" id="lit_block">
-	<td>Selected Literature <br/> for new injection method:</td>
-	<td id="literature"></td>
+	<tr id="auto_block">
+		<td>Search for publication<br/> for this injection method </td>
+		<td><input title="Please, start to type a title" type="text" id="autocomplite_auth" class="input"/></td>
+	</tr>
+
+	<tr style="display:none;" id="lit_block">
+		<td>Selected Literature for <br/> new injection method:</td>
+		<td id="literature"></td>
 	
-</tr>
+	</tr>
 
 
-<tr id="auto_block_tracer">
-	<td>Search for tracer<br/> for this injection method <br/> Can't find? <a href="index.php?c=tracers" target="_blank">Check tracers</a> </td>
-	<td><input title="Please, start to type a title" type="text" id="autocomplite_tracer" class="input"/></td>
-</tr>
+	<tr id="auto_block_tracer">
+		<td>Search for tracer<br/> for this injection method <br/> Cannot find? <a href="index.php?c=tracers" target="_blank">Check tracers</a> </td>
+		<td><input title="Please, start to type a title" type="text" id="autocomplite_tracer" class="input"/></td>
+	</tr>
 
-<tr style="display:none;" id="tracer_block">
-	<td>Selected Tracer <br/> for injection method:</td>
-	<td id="tracer"></td>
+	<tr style="display:none;" id="tracer_block">
+		<td>Selected Tracer <br/> for injection method:</td>
+		<td id="tracer"></td>
 	
-</tr>
+	</tr>
 
 
-<?php foreach($fields as $field): ?>
+	<?php foreach($fields as $field): ?>
 
-<?php if (($field->primary_key == 1) OR  ($field->name == 'literature_id') OR ($field->name == 'tracers_id')) continue; ?>
+	<?php if (($field->primary_key == 1) OR  ($field->name == 'literature_id') OR ($field->name == 'tracers_id')) continue; ?>
 
-<tr>
-	<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
+	<tr>
+		<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
 	
-	<?php if ($field->type == 'blob'): ?>
-	<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ></textarea></td>
-	<?php else : ?>
-	<td><input class="input" name="<?php echo $field->name; ?>" value="" size="30" /></td>
-	<?php endif; ?>
+		<?php if ($field->type == 'blob'): ?>
+		<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ></textarea></td>
+		<?php else : ?>
+		<td><input class="input" name="<?php echo $field->name; ?>" value="" size="30" /></td>
+		<?php endif; ?>
 	
-</tr>
-<?php endforeach; ?>
-</table>
+	</tr>
+	<?php endforeach; ?>
+	</table>
 
-<input type="submit" class="submit" value="Insert" />
+	<input type="submit" class="submit" value="Insert" />
 
-</form>
+	</form>
 
-<br/>
+	<br/>
 
-<div id="help_div"></div>
+	<div id="help_div"></div>
 
+	<!-- Java scripts -->
 
-
-<script type="text/javascript">
-//<![CDATA[
-new Autocomplete('autocomplite_auth', { 
-	serviceUrl:'index.php/literature/ajaxAtocomplit', 
-	onSelect: function(value, data){
-		sel_lit_num = data;
-		literature_select();
+	<script type="text/javascript">
+	//<![CDATA[
+	new Autocomplete('autocomplite_auth', { 
+		serviceUrl:'index.php/literature/ajaxAtocomplit', 
+		onSelect: function(value, data){
+			sel_lit_num = data;
+			literature_select();
 		
-	}
- });
+		}
+	 });
 
 
-new Autocomplete('autocomplite_tracer', { 
-	serviceUrl:'index.php/tracers/ajaxAtocomplit', 
-	onSelect: function(value, data){
-		sel_tracer_num = data;
-		tracer_select();
+	new Autocomplete('autocomplite_tracer', { 
+		serviceUrl:'index.php/tracers/ajaxAtocomplit', 
+		onSelect: function(value, data){
+			sel_tracer_num = data;
+			tracer_select();
 		
-	}
- });
- 
-$('frm').onsubmit = function () { return check_form(this)}
+		}
+	 });
+	 
+	$('frm').onsubmit = function () { return check_form(this)}
 
-//]]>
-</script>
+	//]]>
+	</script>
 
 
 <?php endif;?>
 
 
-
-
-
+<!-- Show -->
 
 <?php if($action == 'show'):?>
 
-<p><a href="index.php?c=methods">Back </a></p>
-<p><a href="index.php?c=methods&m=add">Add new Method </a></p>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
-<h2>All methods</h2>
+	<h1>All methods</h1>
 
-<table border="0" cellpadding="3" cellspacing="1">
-<tr><td>tracer name</td><td>injection method</td><td>survival time</td><td></td></tr>
-<?php foreach($block_data->result() as $bdata): ?>
-<tr>
-	<td><?=$bdata->tracers_name?></td>
-	<td><?=$bdata->injection_method?></td>
-	<td><?=$bdata->survival_time?></td> 
-	<td><a href="index.php?c=methods&m=edit&id=<?=$bdata->methods_id?>">edit</a></td>
-</tr>
-<?php endforeach; ?>
-</table>
+	<p><a href="index.php?c=methods&m=add">Add new Method </a></p>
+
+	<table border="0" cellpadding="3" cellspacing="1">
+	<tr><td><strong>Tracer name</strong></td><td><strong>Injection method</strong></td><td><strong>Survival time</strong></td><td></td></tr>
+	<?php foreach($block_data->result() as $bdata): ?>
+	<tr>
+		<td><?=$bdata->tracers_name?></td>
+		<td><?=$bdata->injection_method?></td>
+		<td><?=$bdata->survival_time?></td> 
+		<td><a href="index.php?c=methods&m=edit&id=<?=$bdata->methods_id?>">edit</a></td>
+	</tr>
+	<?php endforeach; ?>
+	</table>
+
 <?php endif;?>
 
 
-
-
+<!-- Edit -->
 
 <?php if($action == 'edit'):?>
 
-<p><a href="javascript:history.go(-1)" >Back</a></p>
+	<p align="right"><a href="javascript:history.go(-1)">Back</a> <!-- Back button -->
 
-<h2>Edit Injection Method</h2>
+	<h1>Edit Injection Method</h1>
 
-<?php if(isset($block_data)):?>
+	<?php if(isset($block_data)):?>
+
+		<!-- Form -->
+
+		<a href="#" onclick="show_coding_rules('methods'); return false;">Coding rules for Methods</a>
+
+		<form method="post" id="frm" name="frm" action="index.php?c=methods&m=update&id=<?=$block_data->methods_id?>">
+
+		<table border="0" cellpadding="3" cellspacing="1">
 
 
-<a href="#" onclick="show_coding_rules('methods'); return false;">Coding rules for Methods</a>
+		<tr style="display:none;" id="auto_block">
+			<td>Search for publication<br/> for this injection method </td>
+			<td><input title="Please, start to type a title" type="text" id="autocomplite_auth" class="input"/></td>
+		</tr>
 
-<form method="post" id="frm" name="frm" action="index.php?c=methods&m=update&id=<?=$block_data->methods_id?>">
-
-<table border="0" cellpadding="3" cellspacing="1">
-
-
-<tr style="display:none;" id="auto_block">
-	<td>Search for publication<br/> for this injection method </td>
-	<td><input title="Please, start to type a title" type="text" id="autocomplite_auth" class="input"/></td>
-</tr>
-
-<tr  id="lit_block">
-	<td>Selected Literature <br/> for new injection method:</td>
-	<td id="literature">
-		<span id="<?=$block_data->literature_id?>">
-		<input type="hidden" name="literature_id" value="<?=$block_data->literature_id?>">
-		<?=$block_data->literature_title?> <?=$block_data->literature_index?>
-		<a href="#" onclick="lit_replace('<?=$block_data->literature_id?>'); return false;"> Replace</a>
-		</span>
+		<tr  id="lit_block">
+			<td>Selected Literature for <br/> new injection method:</td>
+			<td id="literature">
+				<span id="<?=$block_data->literature_id?>">
+				<input type="hidden" name="literature_id" value="<?=$block_data->literature_id?>">
+				<?=$block_data->literature_title?> <?=$block_data->literature_index?>
+				<a href="#" onclick="lit_replace('<?=$block_data->literature_id?>'); return false;"> Replace</a>
+				</span>
 	
-	</td>
-</tr>
+			</td>
+		</tr>
 
 
-<tr style="display:none;" id="auto_block_tracer">
-	<td>Search for tracer<br/> for this injection method <br/> Can't find? <a target="_blank" href="index.php?c=tracers" target="_blank">Check tracers</a> </td>
-	<td><input title="Please, start to type a title" type="text" id="autocomplite_tracer" class="input"/></td>
-</tr>
+		<tr style="display:none;" id="auto_block_tracer">
+			<td>Search for tracer<br/> for this injection method <br/> Can't find? <a target="_blank" href="index.php?c=tracers" target="_blank">Check tracers</a> </td>
+			<td><input title="Please, start to type a title" type="text" id="autocomplite_tracer" class="input"/></td>
+		</tr>
 
 
-<tr id="tracer_block">
-	<td>Selected Tracer <br/> for injection method:</td>
-	<td id="tracer">
-	<span id="tracer_<?=$block_data->tracers_id?>">
-	<input type="hidden" name="tracers_id" value="<?=$block_data->tracers_id?>"><?=$block_data->tracers_name?>
-	<a href="#" onclick="tracer_replace('<?=$block_data->tracers_id?>'); return false;"> Replace</a>
-	</span>
+		<tr id="tracer_block">
+			<td>Selected Tracer for <br/> injection method:</td>
+			<td id="tracer">
+			<span id="tracer_<?=$block_data->tracers_id?>">
+			<input type="hidden" name="tracers_id" value="<?=$block_data->tracers_id?>"><?=$block_data->tracers_name?>
+			<a href="#" onclick="tracer_replace('<?=$block_data->tracers_id?>'); return false;"> Replace</a>
+			</span>
 	
-	</td>
-</tr>
+			</td>
+		</tr>
 
 
 
 
-<?php foreach($fields as $field): ?>
+		<?php foreach($fields as $field): ?>
 
-<?php if (($field->primary_key == 1) OR ($field->name == 'literature_id') OR ($field->name == 'tracers_id')) continue; ?>
+			<?php if (($field->primary_key == 1) OR ($field->name == 'literature_id') OR ($field->name == 'tracers_id')) continue; ?>
 
-<tr>
-	<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
+			<tr>
+				<td><?php $fname = explode("_", $field->name); foreach ($fname as $fn) { echo $fn." "; }; echo ' '.$field->default; ?></td>
 	
-	<?php if ($field->type == 'blob'): ?>
-	<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ><?php $f=$field->name; echo form_prep($block_data->$f); ?></textarea></td>
-	<?php else : ?>
-	<td><input class="input" name="<?php echo $field->name; ?>" value="<?php $f=$field->name; echo form_prep($block_data->$f); ?>" size="30" /></td>
-	<?php endif; ?>
+				<?php if ($field->type == 'blob'): ?>
+					<td><textarea class="textarea" name="<?php echo $field->name; ?>" cols="30" rows="10" ><?php $f=$field->name; echo form_prep($block_data->$f); ?></textarea></td>
+				<?php else : ?>
+					<td><input class="input" name="<?php echo $field->name; ?>" value="<?php $f=$field->name; echo form_prep($block_data->$f); ?>" size="30" /></td>
+				<?php endif; ?>
 	
-</tr>
-<?php endforeach; ?>
-</table>
+			</tr>
 
-<input type="submit" class="submit" value="Update" />
-
-</form>
-
-<div id="help_div"></div>
-
-
-<script type="text/javascript">
-//<![CDATA[
-new Autocomplete('autocomplite_auth', { 
-	serviceUrl:'index.php/literature/ajaxAtocomplit', 
-	onSelect: function(value, data){
-		sel_lit_num = data;
-		literature_select();
+		<?php endforeach; ?>
 		
-	}
- });
+		</table>
+
+		<input type="submit" class="submit" value="Update" />
+
+		</form>
+
+		<div id="help_div"></div>
 
 
-new Autocomplete('autocomplite_tracer', { 
-	serviceUrl:'index.php/tracers/ajaxAtocomplit', 
-	onSelect: function(value, data){
-		sel_tracer_num = data;
-		tracer_select();
+		<!-- Java scripts -->
+
+		<script type="text/javascript">
+		//<![CDATA[
+		new Autocomplete('autocomplite_auth', { 
+			serviceUrl:'index.php/literature/ajaxAtocomplit', 
+			onSelect: function(value, data){
+				sel_lit_num = data;
+				literature_select();
 		
-	}
- });
+			}
+		 });
 
-sel_lit_num = <?=$block_data->literature_id?>;
-sel_tracer_num = <?=$block_data->tracers_id?>;
 
- 
-$('frm').onsubmit = function () { return check_form(this)}
+		new Autocomplete('autocomplite_tracer', { 
+			serviceUrl:'index.php/tracers/ajaxAtocomplit', 
+			onSelect: function(value, data){
+				sel_tracer_num = data;
+				tracer_select();
+		
+			}
+		 });
 
-//]]>
-</script>
+		sel_lit_num = <?=$block_data->literature_id?>;
+		sel_tracer_num = <?=$block_data->tracers_id?>;
+
+		 
+		$('frm').onsubmit = function () { return check_form(this)}
+
+		//]]>
+		</script>
+
+	<?php endif;?>
 
 <?php endif;?>
 
-<?php endif;?>
 
-
+<!-- Load the footer -->
 
 <?php $this->load->view('footer');
