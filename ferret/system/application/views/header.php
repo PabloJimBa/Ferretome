5fa20554889089e1874f46fe1fee8e5285aff7d2
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-03-10 12:05:59 +0100

    Update header.php

--- ferret/system/application/views/header.php
+++ ferret/system/application/views/header.php
@@ -5,147 +5,144 @@
 
 <head>
 
-<!-- If extraTitle is set, it will be shown. If not, "Ferret Brain DB" will be shown*/ -->
+	<!-- If extraTitle is set, it will be shown. If not, "Ferret Brain DB" will be shown*/ -->
 
-<?php if(isset($extraTitle)):?>
-	<title><?=$extraTitle?></title>
-<?php else:?>
- 	<title>Ferret Brain DB</title>
-<?php endif;?>
+	<?php if(isset($extraTitle)):?>
+		<title><?=$extraTitle?></title>
+	<?php else:?>
+	 	<title>Ferret Brain DB</title>
+	<?php endif;?>
 
-<!-- If extraHeader is set, it will be shown. -->
+	<!-- If extraHeader is set, it will be shown. -->
 
-<?php if(isset($extraHeader)):?>
- <?=$extraHeader?>
- <?php endif;?>
+	<?php if(isset($extraHeader)):?>
+		<?=$extraHeader?>
+	<?php endif;?>
 
-<!-- Style sheet used: -->
+	<!-- Style sheet used: -->
 
-<link href="style.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="js/prototype.js"></script>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="js/prototype.js"></script>
 
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv='expires' content='-1' />
-<meta http-equiv= 'pragma' content='no-cache' />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv='expires' content='-1' />
+	<meta http-equiv= 'pragma' content='no-cache' />
 
 
 </head>
 
 <body>
 
-<!-- Main page header -->
+	<!-- Main page header -->
 
-<div id="header">
-<?php if(!isset($login_token)):?>
-<div id="header_left">
-<h3><a href="index.php?c=welcome"> Main menu</a></h3>
-</div>
-<div id="header_right">
+	<div id="header">
+	<?php if(!isset($login_token)):?>
+		<div id="header_left">
+		<h3><a href="index.php?c=welcome"> Main menu</a></h3>
+		</div>
+		<div id="header_right">
 
-<a href="index.php?c=workflow&type=input">Input queue <?php if(isset($input_queue_number)):?>(<?=$input_queue_number?>)<?php endif;?></a> &nbsp; | &nbsp; 
-<a href="index.php?c=workflow&type=proofreading">Proofreading queue <?php if(isset($proof_queue_number)):?>(<?=$proof_queue_number?>)<?php endif;?></a>  &nbsp; | &nbsp; 
-<strong>Ferret Brain Collation DB</strong>  &nbsp; | &nbsp; 
-<a href="index.php?c=login&m=signout"> Exit</a>
-</div>
+		<a href="index.php?c=workflow&type=input">Input queue <?php if(isset($input_queue_number)):?>(<?=$input_queue_number?>)<?php endif;?></a> &nbsp; | &nbsp; 
+		<a href="index.php?c=workflow&type=proofreading">Proofreading queue <?php if(isset($proof_queue_number)):?>(<?=$proof_queue_number?>)<?php endif;?></a>  &nbsp; | &nbsp; 
+		<strong>Ferret Brain Collation DB</strong>  &nbsp; | &nbsp; 
+		<a href="index.php?c=login&m=signout"> Exit</a>
+		</div>
 
-<!-- Log in page header -->
+		<!-- Log in page header -->
 
-<?php else:?>
+	<?php else:?>
 
-<div id="header_left">
-<h3>Ferret DB login page</h3>
-</div>
+		<div id="header_left">
+		<h3>Ferret DB login page</h3>
+		</div>
 
-<!-- End of log in page header -->
+		<!-- End of log in page header -->
 
-<?php endif;?>
+	<?php endif;?>
 
-</div>
+	</div>
 
-<br clear="all">
+	<br clear="all">
 
-<table id="main_contaner">
-<tr>
-<?php if(!isset($login_token)):?>
-	<td valign="top" >
-<div id="outer_left">
+	<table id="main_contaner">
+	<tr>
+	<?php if(!isset($login_token)):?>
+		<td valign="top" >
+		<div id="outer_left">
 
-<!-- Side menu -->
+	<!-- Side menu -->
 
-<?php if(isset($leftMenu)):?>
+	<?php if(isset($leftMenu)):?>
 
-	<nav>
-			<ul>
-				<li><a href="#">Literature</a>
-				<div>
- 					<ul>
-						<li><a href="index.php?c=literature">Literature</a></li>
-						<li><a href="">Author</a></li>
-						<li><a href="">Journla/Book/Etc</a></li>
-					</ul>
-				</div>
-				<li><a href="#">Mapping</a>
- 				<div>
- 					<ul>
-						<li><a href="">Brain map</a></li>
-						<li><a href="">Brain site</a></li>
-						<li><a href="">Acronym</a></li>
-						<li><a href="">Architecture of BSite</a></li>
-						<li><a href="">Architecture Parameter</a></li>
-						<li><a href="">Relations of BSites</a></li>
-					</ul>
-				</div>
- 				</li>
-				<li><a href="#">Experiment</a>
-				<div>
- 					<ul>
-						<li><a href="">Injection</a></li>
-						<li><a href="">Method of Injection</a></li>
-						<li><a href="">Tracer</a></li>
-						<li><a href="">Data for Injection</a></li>
-						<li><a href="">Data Parameter</a></li>
-						<li><a href="">Labeling Outcome</a></li>
-						<li><a href="">Labelled Site</a></li>
-						<li><a href="">Injection-Outcome Relation</a></li>
-					</ul>
-				</div>
-				<li><a href="#">Special</a>
-				<div>
- 					<ul>
-						<li><a href="">Actions Journal</a></li>
-						<li><a href="">Coding Rules</a></li>
-						<li><a href="">Left menu Editor</a></li>
-						<li><a href="">Experiment</a></li>
-					</ul>
-				</div>
-			</ul>
-	</nav>
+		<nav>
+				<ul>
+					<li><a href="#">Literature</a>
+					<div>
+	 					<ul>
+							<li><a href="index.php?c=literature">Literature</a></li>
+							<li><a href="index.php?c=authors">Authors</a></li>
+							<li><a href="index.php?c=abbreviations">Journals/Books/Etc.</a></li>
+						</ul>
+					</div>
+					<li><a href="#">Mapping</a>
+	 				<div>
+	 					<ul>
+							<li><a href="index.php?c=brainmaps">Brain maps</a></li>
+							<li><a href="index.php?c=brainsites">Brain sites</a></li>
+							<li><a href="index.php?c=acronyms">Acronyms</a></li>
+							<li><a href="index.php?c=architecture">Architecture of BSite</a></li>
+							<li><a href="index.php?c=mapsrelations">Relations of BSites</a></li>
+						</ul>
+					</div>
+	 				</li>
+					<li><a href="#">Experiment</a>
+					<div>
+	 					<ul>
+							<li><a href="index.php?c=injections">Injections</a></li>
+							<li><a href="index.php?c=methods">Methods of Injection</a></li>
+							<li><a href="index.php?c=tracers">Tracers</a></li>
+							<li><a href="index.php?c=injectionsdata">Data for Injection</a></li>
+							<li><a href="index.php?c=injectionsparameters">Data Parameters</a></li>
+							<li><a href="index.php?c=labelingoutcome">Labeling Outcomes</a></li>
+							<li><a href="index.php?c=labelledsites">Labelled Sites</a></li>
+							<li><a href="index.php?c=injectionsoutcomes&m=add">Injection-Outcome Relations</a></li>
+						</ul>
+					</div>
+					<li><a href="#">Special</a>
+					<div>
+	 					<ul>
+							<li><a href="index.php?c=journal">Actions Journal</a></li>
+							<li><a href="index.php?c=codingrules">Coding Rules</a></li>
+						</ul>
+					</div>
+				</ul>
+		</nav>
 
 
 	
-<!--
-	<?php foreach ($leftMenu->result() as $lmenu):?>
+	<!--
+		<?php foreach ($leftMenu->result() as $lmenu):?>
 					
-		<?php if($lmenu->item_type == '0'):?>
-		<p><strong><?=$lmenu->item_caption?></strong></p>
-		<?php endif;?>
+			<?php if($lmenu->item_type == '0'):?>
+			<p><strong><?=$lmenu->item_caption?></strong></p>
+			<?php endif;?>
 						
-		<?php if($lmenu->item_type == '1'):?>
-		<p><a href="<?=$lmenu->item_link?>"><?=$lmenu->item_caption?></a></p>
-		<?php endif;?>
+			<?php if($lmenu->item_type == '1'):?>
+			<p><a href="<?=$lmenu->item_link?>"><?=$lmenu->item_caption?></a></p>
+			<?php endif;?>
 					
 					
-	<?php endforeach;?> 
--->
+		<?php endforeach;?> 
+	-->
 
-<?php endif;?>
+	<?php endif;?>
 	
-</div>
+	</div>
 
-	</td>
-<?php endif;?>
+		</td>
+	<?php endif;?>
 	
-	<td valign="top">
+		<td valign="top">
 
-<div id="outer_right">
+	<div id="outer_right">
