63a945f1d9171640956c4ac64cc5cf04060fd03c
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-04-08 13:11:29 +0200

    Update manual.php

--- ferret/system/application/controllers/manual.php
+++ ferret/system/application/controllers/manual.php
@@ -21,4 +21,94 @@ class Manual extends Controller {
 		$this->load->view('manual',$this->data);
 
 	}
+	
+	function literature_add()
+	{
+		$this->data['action'] = 'literature_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function author_add()
+	{
+		$this->data['action'] = 'author_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function journal_add()
+	{
+		$this->data['action'] = 'journal_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function acronym_add()
+	{
+		$this->data['action'] = 'acronym_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function brainmap_add()
+	{
+		$this->data['action'] = 'brainmap_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function brainsite_add()
+	{
+		$this->data['action'] = 'brainsite_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function architecture_add()
+	{
+		$this->data['action'] = 'architecture_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function bmrelation_add()
+	{
+		$this->data['action'] = 'bmrelation_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function injectionmethod_add()
+	{
+		$this->data['action'] = 'injectionmethod_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function injection_add()
+	{
+		$this->data['action'] = 'injection_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function injectiondata_add()
+	{
+		$this->data['action'] = 'injectiondata_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function outcome_add()
+	{
+		$this->data['action'] = 'outcome_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function labelledsite_add()
+	{
+		$this->data['action'] = 'labelledsite_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function injectionoutcome_add()
+	{
+		$this->data['action'] = 'injectionoutcome_add';
+		$this->load->view('manual',$this->data);
+	}
+
+	function parameter_add()
+	{
+		$this->data['action'] = 'parameter_add';
+		$this->load->view('manual',$this->data);
+	}
 }
