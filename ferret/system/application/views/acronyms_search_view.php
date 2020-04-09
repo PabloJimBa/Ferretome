23cb640e0e1ba06e15425f3be16dfd07259f0421
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-04-09 10:47:59 +0200

    Update acronyms_search_view.php

--- ferret/system/application/views/acronyms_search_view.php
+++ ferret/system/application/views/acronyms_search_view.php
@@ -3,8 +3,7 @@
 <tr><td>Acronym shortname</td> <td>Acronym fullname</td> <td></td></tr>
 <tr><td><?=$acr_data->acronym_name?></td> <td><?=$acr_data->acronym_full_name?></td> 
 
-<td><a href="index.php?c=acronyms&m=edit&id=<?=$acr_data->brain_site_acronyms_id?>">details</a></td>
+<td><a href="index.php?c=acronyms&m=edit&id=<?=$acr_data->brain_site_acronyms_id?>">details</a> &nbsp; | &nbsp; <a href="index.php?c=acronyms&m=confirm&id=<?=$acr_data->brain_site_acronyms_id?>">delete</a></td>
 
 </tr>
 </table>
-
