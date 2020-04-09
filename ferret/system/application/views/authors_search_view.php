db289e2cd47a4f7e7da795098100b64c7b615b4f
PabloJB <54323180+PabloJimBa@users.noreply.github.com>
2020-04-09 10:49:38 +0200

    Update authors_search_view.php

--- ferret/system/application/views/authors_search_view.php
+++ ferret/system/application/views/authors_search_view.php
@@ -3,8 +3,7 @@
 <tr><td>Author Surname</td> <td>Author Name</td> <td>Author Middlename</td><td></td></tr>
 <tr><td><?=$auth_data->authors_surname?></td> <td><?=$auth_data->authors_name?></td> <td><?=$auth_data->authors_middleName?></td>
 
-<td><a target="_blank" href="index.php?c=authors&m=edit&id=<?=$auth_data->authors_id?>">details</a></td>
+<td><a target="_blank" href="index.php?c=authors&m=edit&id=<?=$auth_data->authors_id?>">details</a> &nbsp; | &nbsp; <a href="index.php?c=authors&m=confirm&id=<?=$auth_data->authors_id?>">delete</a></td>
 
 </tr>
 </table>
-
